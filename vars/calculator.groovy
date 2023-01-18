f627ab7e84c17d383076011b4c94d0588c7e400f
kirantamatam <95377582+kirantamatam@users.noreply.github.com>
2023-01-18 21:31:51 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -4,3 +4,9 @@ def add(x,y){
 def mul(x,y){
   echo "multiplication of ${x} and ${y} is ${x*y}"
 }
+def sub(x,y){
+  echo "subtraction of ${x} and ${y} is ${x-y}"
+}
+def div(x,y){
+  echo "divison of ${x} and ${y} is ${x/y}"
+}
